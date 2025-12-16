23342d98090c17791ea48ae5e9a407b8ed3ac61a
Keven Costa <ogkeven278@gmail.com>
2025-12-16 14:11:11 -0300

    chore: Configure main.ts for Swagger, Global Validation Pipe, and CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+
+  // Indicamos a configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Farmácia')
+    .setDescription('Projeto Farmácia')
+    .setContact("Generation Brasil", "http://www.generationbrasil.online", "generation@email.com")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  // Ajustando o Fuso Horário do BD
+  process.env.TZ = '-03:00';
+
+  // Aplicando os recursos de validação
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Habilitando o CORS do projeto
+  app.enableCors();
+
+  // Indico qual porta o projeto está sendo executado
+  await app.listen(4000);
 }
+
 bootstrap();
